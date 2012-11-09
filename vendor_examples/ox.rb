af04f3383cd0d9bf58b86e490d30be82e72fd93d
Noah Luck Easterly <noah.easterly@gmail.com>
2012-11-08 20:43:05 -0500

    add location for examples of use of various vendor libraries

---
+++ vendor_examples/ox.rb
@@ -0,0 +1,48 @@
+require 'stringio'
+require 'ox'
+
+class Sample < ::Ox::Sax
+  def initialize
+    @indentation = 0
+  end
+  def show msg
+    print("  " * @indentation)
+    puts msg
+  end
+  private :show
+  def start_element(name)
+    show "start: #{name}"
+    @indentation += 1
+	end
+  def end_element(name)
+    @indentation -= 1
+    show "end: #{name}"
+	end
+  def attr(name, value)
+    show "- #{name} => #{value}"
+	end
+  def text(value)
+    show "text: #{value.inspect}"
+	end
+end
+
+io = StringIO.new(%{
+<top name="sample" value="keylime">
+  <middle name="second">
+    <bottom name="third">&#8;</bottom>
+  </middle>
+</top>
+})
+
+handler = Sample.new()
+Ox.sax_parse(handler, io)
+# outputs
+# start: top
+#   name => sample
+# start: middle
+#   name => second
+# start: bottom
+#   name => third
+# end: bottom
+# end: middle
+# end: top
